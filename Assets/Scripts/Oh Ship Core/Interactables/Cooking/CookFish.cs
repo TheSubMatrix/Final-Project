db85317b10fd0270535ef138c53e3a2262d7b64d
Irina Mishina <irinamishina131@gmail.com>
2026-06-05 00:38:33 -0400

    cooking mechanism (except interaction)

--- Assets/Scripts/Oh Ship Core/Interactables/Cooking/CookFish.cs
+++ Assets/Scripts/Oh Ship Core/Interactables/Cooking/CookFish.cs
@@ -1,49 +1,95 @@
+using System.Collections;
 using UnityEngine;
 
 public class CookFish : MonoBehaviour
 {
     Material material;
-    float timer = 0;
     float cookedAmount;
+    bool isCooking = false;
+    bool burned = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         material = GetComponent<Renderer>().material;
-        float cookedAmount = material.GetFloat("_Cooked_Amount");
+        cookedAmount = material.GetFloat("_Cooked_Amount");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        cookedAmount = material.GetFloat("_Cooked_Amount");
+        //cookedAmount = material.GetFloat("_Cooked_Amount");
         Debug.Log("Cook:" + cookedAmount);
-
+        CheckForStove();
         CheckCookedStatus();
-    }
 
-    private void OnCollisionStay(Collision collision)
-    {
-        if(collision.gameObject.tag == "Stove")
+        if(isCooking)
         {
             StartCooking();
         }
     }
 
+    private void OnCollisionStay(Collision collision)
+    {
+
+    }
+
     private void StartCooking()
     {
-        timer = 0;
-        int timerInInt = (int)timer;
-        cookedAmount += 0.1f * Time.deltaTime;
+        if(cookedAmount < 0.7f)
+        {
+            cookedAmount += 0.1f * Time.deltaTime;
+            material.SetFloat("_Cooked_Amount", cookedAmount);
+        }
+
+        if(cookedAmount >= 0.7f)
+        {
+            burned = true;
+            StartCoroutine(Burn());
+        }
     }
 
     void EndCooking()
     {
-
+        isCooking = false;
+        Destroy(gameObject);
     }
 
     private void CheckCookedStatus()
     {
+
+    }
+
+    private void CheckForStove()
+    {
+        Vector3 origin = transform.position;
+        Vector3 direction = Vector3.down;
+
+        RaycastHit hit;
+
+        Debug.DrawRay(origin, direction * 0.1f, Color.red);
+
+        if (Physics.Raycast(origin, direction, out hit, 0.1f))
+        {
+            if (hit.collider.CompareTag("Stove"))
+            {
+                if(!isCooking)
+                {
+                    isCooking = true;
+                }
+            }
+        }
+    }
+
+    private IEnumerator Burn()
+    {
+        yield return new WaitForSeconds(3);
+        cookedAmount += 0.1f * Time.deltaTime;
+        material.SetFloat("_Cooked_Amount", cookedAmount);
+
+        if(cookedAmount >= 1)
+        {
+            EndCooking();
+        }
 
     }
 }
