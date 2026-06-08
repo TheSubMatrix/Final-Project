ba485f65b87f78305eeffb2aa4d4125f79f78d98
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2026-06-08 12:39:13 -0400

    Changed Build scene

---
+++ Assets/Scripts/Oh Ship Core/Ship/Coal/CoalUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CoalUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public GameObject DisplayInputButton(string buttonName)
+    {
+        return new GameObject();
+    }
+
+    private void CorrectButtonPressed()
+    {
+        
+    }
+
+    private void IncorrectButtonPressed()
+    {
+        
+    }
+
+    private void HideUI()
+    {
+        
+    }
+    
+}
