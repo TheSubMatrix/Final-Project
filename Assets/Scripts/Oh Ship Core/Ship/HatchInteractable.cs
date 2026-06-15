237ef0418f4c74f1ce43d15c8f09bba14c53ae47
Colin Whiteford <colinwhiteford516@gmail.com>
2026-06-15 16:08:21 -0400

    Hatch Door Animation

--- Assets/Scripts/Oh Ship Core/Ship/HatchInteractable.cs
+++ Assets/Scripts/Oh Ship Core/Ship/HatchInteractable.cs
@@ -1,6 +1,8 @@
 using MatrixUtils.Attributes;
 using MatrixUtils.AudioSystem;
 using UnityEngine;
+using UnityEngine.Events;
+
 /// <summary>
 /// Interacting with this object will move the <see cref="IInteractor"/> to the position specified in the inspector
 /// </summary>
@@ -10,11 +12,13 @@ public class HatchInteractable : MonoBehaviour, IInteractable, IPromptProvider
     [SerializeField] SoundData m_interactSound;
     [SerializeField] Transform m_widgetPosition;
     [SerializeField] Transform m_outPosition;
+    [SerializeField] UnityEvent m_onInteract;
     public InteractionSession BeginInteraction(IInteractor interactor)
     {
         ITransitionHider transitionHider = interactor.GetAssociatedGameObject().transform.root.GetComponent<IPlayerControllable>().GetActivePlayerController().GetAssociatedGameObject().transform.root.GetComponentInChildren<ITransitionHider>();
         Rigidbody rb = interactor.GetAssociatedGameObject().transform.root.GetComponent<Rigidbody>();
         SoundManager.Instance.CreateSound().WithSoundData(m_interactSound).AttachedTo(transform).WithRandomPitch().Play();
+        m_onInteract.Invoke();
         transitionHider.FadeIn(0.2f, () =>
         {
             rb.MovePosition(m_outPosition.position);
@@ -24,5 +28,5 @@ public class HatchInteractable : MonoBehaviour, IInteractable, IPromptProvider
     }
 
     public PromptData GetPromptData() => m_promptData;
-    public Vector3 GetWidgetWorldPosition() => m_widgetPosition == null? transform.position : m_widgetPosition.position;
+    public Vector3 GetWidgetWorldPosition() => !m_widgetPosition? transform.position : m_widgetPosition.position;
 }
