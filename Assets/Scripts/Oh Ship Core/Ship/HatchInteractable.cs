1457b030a62767de92f9a953433114e1a0d5ad78
Colin Whiteford <colinwhiteford516@gmail.com>
2026-06-15 15:46:37 -0400

    Fake Water Fill Offset

--- Assets/Scripts/Oh Ship Core/Ship/HatchInteractable.cs
+++ Assets/Scripts/Oh Ship Core/Ship/HatchInteractable.cs
@@ -1,4 +1,5 @@
 using MatrixUtils.Attributes;
+using MatrixUtils.AudioSystem;
 using UnityEngine;
 /// <summary>
 /// Interacting with this object will move the <see cref="IInteractor"/> to the position specified in the inspector
@@ -6,12 +7,14 @@ using UnityEngine;
 public class HatchInteractable : MonoBehaviour, IInteractable, IPromptProvider
 {
     [SerializeReference, ClassSelector] PromptData m_promptData;
+    [SerializeField] SoundData m_interactSound;
     [SerializeField] Transform m_widgetPosition;
     [SerializeField] Transform m_outPosition;
     public InteractionSession BeginInteraction(IInteractor interactor)
     {
         ITransitionHider transitionHider = interactor.GetAssociatedGameObject().transform.root.GetComponent<IPlayerControllable>().GetActivePlayerController().GetAssociatedGameObject().transform.root.GetComponentInChildren<ITransitionHider>();
         Rigidbody rb = interactor.GetAssociatedGameObject().transform.root.GetComponent<Rigidbody>();
+        SoundManager.Instance.CreateSound().WithSoundData(m_interactSound).AttachedTo(transform).WithRandomPitch().Play();
         transitionHider.FadeIn(0.2f, () =>
         {
             rb.MovePosition(m_outPosition.position);
