e8f2bf93480b4cb0b5f262612585612dc8290f4e
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2026-06-17 18:38:12 -0400

    Underbelly follow

--- Assets/Scripts/Oh Ship Core/Spline Movement/Underbelly Follow.cs
+++ Assets/Scripts/Oh Ship Core/Spline Movement/Underbelly Follow.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -5,20 +6,21 @@ public class UnderbellyFollow : MonoBehaviour
 {
     [SerializeField] private float _speed = 5.0f;
     [SerializeField] private GameObject _playerSteamBoat;
-    private Rigidbody _rigidbody;
-    private SplineContainer _spline;
-    private float _underBellyProgress;
-    void Start()
+    [SerializeField] private float yOffset;
+    private Rigidbody m_rigidbody;
+
+
+    private void Start()
     {
-        _spline = FindAnyObjectByType<SplineContainer>();
-        _rigidbody = GetComponent<Rigidbody>();
+        m_rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        _underBellyProgress += _speed / _spline.CalculateLength() * Time.deltaTime;
-        transform.position = _spline.EvaluatePosition(_underBellyProgress);
-        _rigidbody.MovePosition(transform.position);
+        Vector3 targetPosition = _playerSteamBoat.transform.position;
+        targetPosition.y -= yOffset; 
+        m_rigidbody.MovePosition(targetPosition);
+        m_rigidbody.MoveRotation(_playerSteamBoat.transform.rotation);
     }
 }
